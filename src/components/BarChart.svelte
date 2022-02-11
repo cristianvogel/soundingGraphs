e069844d228b8a717243bc5d7096e76d4c642244
cristianvogel <cristian.vogel55@gmail.com>
2022-02-11 17:48:15 +0100

    attempting to get Plot to just work

---
+++ src/components/BarChart.svelte
@@ -0,0 +1,23 @@
+<script>
+    import * as Plot from "@observablehq/plot";
+    import {onMount} from "svelte";
+
+    export let data
+    let graphPlot
+
+    onMount( () => {
+     //read svg
+        const sales = [
+            {units: 10, fruit: "fig"},
+            {units: 20, fruit: "date"},
+            {units: 40, fruit: "plum"},
+            {units: 30, fruit: "plum"}
+        ]
+        graphPlot = (Plot.dot(sales, {x: "units", y: "fruit"}).plot())
+
+
+    })
+
+</script>
+
+<object type="image/svg+xml" data={graphPlot}></object>
